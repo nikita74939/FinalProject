995e8b97833178b585ed91ea85ad2af350720d0f
Bintang Jati <bintangid69@gmail.com>
2024-11-14 12:29:01 +0700

    koneksi ke database

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,12 @@
 <?php
     $hostname = "localhost";
+    $user = 'root';
+    $pass = '';
+    $db = 'beautyrecipe';
+    $conn = mysqli_connect($hostname, $user, $pass, $db);
+    if($conn) {
+        echo "koneksi berhasil";
+    }
+
+    mysqli_select_db($conn, $db);
 ?>
