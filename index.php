50b5dab049b1f0d3e4edd7d8debea9229c92625b
nikita74939 <itanikita20005@gmail.com>
2024-11-14 13:40:11 +0700

    kurang gambar+js

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
                     </li>
                 </ul>
                 <span class="navbar-text">
-                    <a href="login.php" class="me-3">
+                    <a href="#login" class="me-3">
                         <img src="img/user.png" alt="" class="img-fluid align-top me-1" width="25" height="20"
                             style="font-family: 'Quicksand';">
                     </a>
@@ -83,8 +83,9 @@
             <div class="row mx-5">
                 <div class="col-4">
                     <div class="card mx-3" style="border: none; background-color: white">
-                        <img src="img/satu.png" class="card-img-top px-5 pt-3" width="50%" alt="...">
-                        <div class="card-body pb-5">
+                        <img src="img/satu.png" class="card-img-top pt-3" width="50%" alt="..."
+                            style="padding-left:70px; padding-right:70px;">
+                        <div class="card-body">
                             <h5 class="card-title">DIY Recipe</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk
                                 of the card's content.</p>
@@ -93,8 +94,9 @@
                 </div>
                 <div class="col-4">
                     <div class="card mx-3" style="border: none; background-color: white">
-                        <img src="img/dua.png" class="card-img-top px-5 pt-3" width="50%" alt="...">
-                        <div class="card-body pb-5">
+                        <img src="img/dua.png" class="card-img-top pt-3" width="50%" alt="..."
+                            style="padding-left:70px; padding-right:70px;">
+                        <div class="card-body">
                             <h5 class="card-title">Share Recipe</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk
                                 of the card's content.</p>
@@ -103,8 +105,9 @@
                 </div>
                 <div class="col-4">
                     <div class="card mx-3" style="border: none; background-color: white">
-                        <img src="img/tiga.png" class="card-img-top px-5 pt-3" width="50%" alt="...">
-                        <div class="card-body pb-5">
+                        <img src="img/tiga.png" class="card-img-top pt-3" width="50%" alt="..."
+                            style="padding-left:70px; padding-right:70px;">
+                        <div class="card-body">
                             <h5 class="card-title">Save Recipe</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk
                                 of the card's content.</p>
@@ -169,35 +172,43 @@
     <!-- most populer end -->
 
     <!-- login -->
-    <div class="login p-5" id="login"  style="display: none">
+    <div class="login p-5 mb-5" id="login" style="display: block">
         <div class="card m-5">
             <div class="card-body">
                 <div class="container">
                     <div class="row">
-                        <div class="col-6 p-5">
-                        
+                        <div class="col-7 p-5">
+
                         </div>
-                        <div class="col-6 p-5">
+                        <div class="col-5 p-4">
                             <h4 style="font-family: 'Quicksand'; font-weight: 400; text-align: center">Login to</h4>
                             <h2 style="text-align: center">Beauty Recipe</h2>
 
-                            <form action="cek_login.php" method="post" class="mt-5 px-5">
+                            <form action="cek_login.php" method="post" class="mt-4 px-5">
                                 <div class="my-2">
-                                    <label for="" class="form-label text-start ms-1" style="font-family: 'Quicksand';">Username</label>
-                                    <input type="text" class="form-control rounded-pill">
-
+                                    <label for="" class="form-label text-start ms-1"
+                                        style="font-family: 'Quicksand';">Username</label>
+                                    <input type="text" class="form-control rounded-pill" name="username" id="usernameLogin" placeholder="Username" style="font-family: 'Quicksand';">
+                                    <div id="usernameHelp" class="form-text" style="font-family: 'Quicksand';">
+                                        Username not found.
+                                    </div>
                                 </div>
-                                <div class="my-4">
-                                    <label for="" class="form-label ms-1" style="font-family: 'Quicksand';">Password</label>
-                                    <input type="password" class="form-control rounded-pill">
-
+                                <div class="my-3">
+                                    <label for="" class="form-label ms-1"
+                                        style="font-family: 'Quicksand';">Password</label>
+                                    <input type="password" class="form-control rounded-pill"  name="password" id="passwordLogin" placeholder="Password" style="font-family: 'Quicksand';">
+                                    <div id="passwordHelp" class="form-text" style="font-family: 'Quicksand';">
+                                        Wrong Password.
+                                    </div>
                                 </div>
 
                                 <div style="text-align: center">
-                                    <input type="submit" class="btn btn-outline-dark mt-3" value="LOGIN" style="background-color: rgb(140, 186, 159);">
+                                    <input type="submit" class="btn btn-outline-dark mt-3" value="LOGIN"
+                                        style="background-color: rgb(140, 186, 159);">
 
                                 </div>
-
+                                
+                                <p class="pt-5" style="font-family: 'Quicksand'; font-weight: 400; font-size: 14px">Haven't an account? Register <a href="#regist" id="regist">here</a></p>
                             </form>
                         </div>
                     </div>
@@ -208,33 +219,48 @@
     <!-- login end -->
 
     <!-- regist -->
-    <div class="register" id="regist"  style="display: block">
-    <div class="card m-5">
+    <div class="register my-5" id="regist" style="display: block">
+        <div class="card m-5">
             <div class="card-body">
                 <div class="container">
                     <div class="row">
-                        <div class="col-6 p-5">
-                        
+                        <div class="col-7 p-5">
+
                         </div>
-                        <div class="col-6 p-5">
-                            <h4 style="font-family: 'Quicksand'; font-weight: 400; text-align: center">Join to</h4>
+                        <div class="col-5 p-4">
                             <h2 style="text-align: center">Beauty Recipe</h2>
 
-                            <form action="cek_login.php" method="post" class="mt-5 px-5">
-                                <div class="my-2">
-                                    <label for="" class="form-label text-start ms-1" style="font-family: 'Quicksand';">Username</label>
-                                    <input type="text" class="form-control rounded-pill">
+                            <form action="regist.php" method="post" class="mt-4 px-5" >
+                                <div class="my-2" >
+                                    <label for="" class="form-label text-start ms-1"
+                                        style="font-family: 'Quicksand';">Username</label>
+                                    <input type="text" class="form-control rounded-pill" name="username" id="usernameRegist" placeholder="Username" style="font-family: 'Quicksand';">
+                                    <div id="usernameHelpR" class="form-text" style="font-family: 'Quicksand';">
+                                        Username already used.
+                                    </div>
 
                                 </div>
                                 <div class="my-4">
-                                    <label for="" class="form-label ms-1" style="font-family: 'Quicksand';">Password</label>
-                                    <input type="password" class="form-control rounded-pill">
+                                    <label for="" class="form-label ms-1"
+                                        style="font-family: 'Quicksand';">Password</label>
+                                    <input type="password" class="form-control rounded-pill" name="password" id="passwordRegist" placeholder="Password" style="font-family: 'Quicksand';">
+                                    <div id="passwordHelpR" class="form-text" style="font-family: 'Quicksand';">
+                                        Your password must be 8-20 characters long.
+                                    </div>
+                                </div>
 
+                                <div class="my-4">
+                                    <label for="" class="form-label ms-1" style="font-family: 'Quicksand';">Confirm
+                                        Password</label>
+                                    <input type="password" class="form-control rounded-pill" id="confirmPassword" placeholder="Confirm Password" style="font-family: 'Quicksand';">
+                                    <div id="ConfirmPasswordHelpR" class="form-text" style="font-family: 'Quicksand';">
+                                        Passwords do not match.
+                                    </div>
                                 </div>
 
                                 <div style="text-align: center">
-                                    <input type="submit" class="btn btn-outline-dark mt-3" value="LOGIN" style="background-color: rgb(140, 186, 159);">
-
+                                    <input type="submit" class="btn btn-outline-dark mt-3" value="Create an account"
+                                        style="background-color: rgb(140, 186, 159);">
                                 </div>
 
                             </form>
@@ -246,8 +272,27 @@
     </div>
     <!-- regist end -->
 
-    <footer class="p-4">
-        <h4 style="font-family: 'Playfair Display'; font-weight: 400;">DIY Beauty Recipe Sharing</h4>
+    <footer class="px-4 pt-4">
+        <div class="container">
+            <h4 class="pt-3" style="font-family: 'Playfair Display'; font-weight: 400;">DIY Beauty Recipe Sharing</h4>
+            <div class="row my-4 pb-5">
+                <div class="col-4">
+                    <h6 class="my-2">MENU</h6>
+                    <p class="my-2" style="font-family: 'Quicksand';">Home</p>
+                    <p class="my-2" style="font-family: 'Quicksand';">Explore</p>
+                    <p class="my-2" style="font-family: 'Quicksand';">Uploud</p>
+                </div>
+                <div class="col-4">
+
+                </div>
+                <div class="col-4">
+                    
+                </div>
+            </div>
+        </div>
+        <div class="mt-5 p-2">
+            halo
+        </div>
     </footer>
 
     <!-- java script-->
@@ -255,7 +300,7 @@
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="js/script.js"></script>
+    <script src="js/scriptIndex.js"></script>
 </body>
 
 </html>
