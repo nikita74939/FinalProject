48ec4c042cb2011f045253829373d45bfdd6a259
nikita74939 <itanikita20005@gmail.com>
2024-11-19 20:32:23 +0700

    footer

--- index.php
+++ index.php
@@ -75,7 +75,7 @@ if ($username) {
                     <div class="mx-3">
                         <h1>Glow Up, Babe!</h1>
                         <h6 class="pt-2">Why spend $$$ when your kitchen’s got the real tea? Whip up your own skincare potions, feel the slay, and serve that natural glow EVERYWHERE. Your skin, your vibe, your rules. PERIOD.</h6>
-                        <a href="#login" class="btn btn-outline-dark mt-3" style="background-color: rgb(140, 186, 159);">Explore Now !</a>
+                        <a href=<?= $username ? "profil.php" : "#login" ?> class="btn btn-outline-dark mt-3" style="background-color: rgb(140, 186, 159);">Explore Now !</a>
                     </div>
                 </div>
                 <div class="col-6 text-end">
@@ -269,7 +269,7 @@ if ($username) {
                 <div class="col-4"></div>
             </div>
         </div>
-        <div class="mt-5 p-2">halo</div>
+        <div class="mt-3 pb-3 text-center">&copy;2024 by Nikita & Bintang</div>
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
